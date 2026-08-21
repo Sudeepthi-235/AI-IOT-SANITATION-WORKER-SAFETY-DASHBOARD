3b1301acad6c3d91a2cd08a887d7d8e26465d9ea
Sudeepthi Pasupuleti <pasupuletisudeepthi235@gmail.com>
2026-08-21 16:25:00 +0530

    Prepare application for deployment

--- app.py
+++ app.py
@@ -683,4 +683,9 @@ def monthly_report():
     return jsonify(report)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    import os
+
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5001))
+    )
